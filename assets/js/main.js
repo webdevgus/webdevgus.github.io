5df1e05f7b669397898aade990bf0404f5105b64
gusbartolini <wwjd607@gmail.com>
2021-01-04 11:34:31 -0600

    updated resume/QR code

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,7 +53,7 @@
 			$nav_li = $nav.find('li');
 
 		// Add "middle" alignment classes if we're dealing with an even number of items.
-			if ($nav_li.length % 2 == 0) {
+			if ($nav_li.length % 3 == 0) {
 
 				$nav.addClass('use-middle');
 				$nav_li.eq( ($nav_li.length / 2) ).addClass('is-middle');
